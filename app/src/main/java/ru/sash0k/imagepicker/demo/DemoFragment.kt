1c6d30096b72a04688e87c8e5ba3279970fc4184
Alexander <Sash0k.V@yandex.ru>
2021-09-29 10:44:11 +0300

    Initial commit
    Based on solution https://github.com/kroegerama/bottomsheet-imagepicker

---
+++ app/src/main/java/ru/sash0k/imagepicker/demo/DemoFragment.kt
@@ -0,0 +1,54 @@
+package ru.sash0k.imagepicker.demo
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import ru.sash0k.imagepicker.BottomSheetImagePicker
+import ru.sash0k.imagepicker.ButtonType
+import ru.sash0k.imagepicker.demo.databinding.FragmentDemoBinding
+
+class DemoFragment : Fragment(), BottomSheetImagePicker.OnImagesSelectedListener {
+
+    private var _binding: FragmentDemoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentDemoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonFirst.setOnClickListener { pickFile("example") }
+    }
+
+    private fun pickFile(tag: String) {
+        BottomSheetImagePicker.Builder("imagepicker.demo")
+            .cameraButton(ButtonType.Tile)
+            .galleryButton(ButtonType.Button)
+            .singleSelectTitle(R.string.imagePickerSingle)
+            .peekHeight(R.dimen.peekHeight)
+            .requestTag(tag)
+            .show(childFragmentManager)
+    }
+
+    override fun onImagesSelected(uris: List<Uri>, tag: String?) {
+        val view = when (tag) {
+            "example" -> binding.firstImage
+            else -> null
+        }
+
+        if (view != null) {
+            uris.firstOrNull()?.let { Glide.with(this).load(it).into(view) }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
