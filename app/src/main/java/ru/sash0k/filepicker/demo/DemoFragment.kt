bb86cd52aae094aedb63acc05aef3c320b0c859f
Alexander <Sash0k.V@yandex.ru>
2021-10-04 13:57:53 +0300

    feat: view multiple selection in demo app

--- app/src/main/java/ru/sash0k/filepicker/demo/DemoFragment.kt
+++ app/src/main/java/ru/sash0k/filepicker/demo/DemoFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.bumptech.glide.Glide
 import ru.sash0k.filepicker.BottomSheetImagePicker
 import ru.sash0k.filepicker.ButtonType
@@ -23,7 +24,7 @@ class DemoFragment : Fragment(), BottomSheetImagePicker.OnImagesSelectedListener
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonFirst.setOnClickListener { pickFile("example") }
+        binding.buttonPick.setOnClickListener { pickFile("example") }
     }
 
     private fun pickFile(tag: String) {
@@ -37,15 +38,11 @@ class DemoFragment : Fragment(), BottomSheetImagePicker.OnImagesSelectedListener
     }
 
     override fun onImagesSelected(uris: List<Uri>, tag: String?) {
-        val view = when (tag) {
-            "example" -> binding.firstImage
-            else -> null
-        }
-
-        if (view != null) {
-            uris.firstOrNull()?.let { Glide.with(this).load(it).into(view) }
-        }
+        uris.firstOrNull()?.let { load(it, binding.firstImage) }
+        if (uris.size > 1) load(uris[1], binding.secondImage)
     }
+
+    private fun load(uri: Uri, view: ImageView) = Glide.with(this).load(uri).error(R.drawable.ic_frame_pdf).into(view)
 
     override fun onDestroyView() {
         super.onDestroyView()
